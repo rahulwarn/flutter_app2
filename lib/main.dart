3cf19b918c70c6d4060e8fdf15696b0314144c29
rahul <rahulwarn@gmail.com>
2020-02-07 04:27:56 +0530

    this is my first flutter commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.amber,
       ),
-      home: Center(child: MyHomePage(title: 'Demo')),
+      home: Center(child: MyHomePage(title: 'Demo 1')),
     );
   }
 }
